c80c0d9cf607ae2a64f573f524095e12ffcd2ed9
Дима Дима <dimabarannik2@gmail.com>
2024-04-07 21:26:52 +0300

    Add project files.

---
+++ EPAM_HW_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_HW_5
+{
+    internal class Program
+    {
+        static double sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("First number: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Second number: ");
+            double b = double.Parse(Console.ReadLine());
+            double c =sum(a, b);
+            Console.WriteLine($"Average: {c / 2}");
+        }
+    }
+}
